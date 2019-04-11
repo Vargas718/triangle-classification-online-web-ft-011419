34c7246a84668e279f808463e4f68319a30c275b
Rafael Vargas <vargas718@gmail.com>
2019-04-11 16:43:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,6 +14,7 @@ class Triangle
       :equilateral
        elsif @side1 == @side2 && @side2 != @side3 || @side2 == @side2 && @side2 != @side1 || @side3 == @side1 && @side2 != @side3
      :isosceles
+      elsif 
         
      
     elsif @side1 <= 0 || @side2 <= 0 || @side3 <= 0 
