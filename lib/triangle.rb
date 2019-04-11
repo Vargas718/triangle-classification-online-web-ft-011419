9ad8a683d6c30cb7b9fcd2d51049359d7132c9a4
Rafael Vargas <vargas718@gmail.com>
2019-04-11 16:54:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   
   def kind
     binding.pry
-   if  @side1 == @side2 && @side2 == @side3
+   if  (@side1 == @side2 && @side2 == @side3) && @side1 > 0
       :equilateral
        elsif @side1 == @side2 && @side2 != @side3 || @side2 == @side3 && @side2 != @side1 || @side3 == @side1 && @side2 != @side3
      :isosceles
