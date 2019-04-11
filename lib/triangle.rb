55fdd932a80b60326bd94ddf5e93f1f73e3947ce
Rafael Vargas <vargas718@gmail.com>
2019-04-11 17:00:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,15 +15,14 @@ class Triangle
     elsif
       @side1 + @side2 > @side3 || @side3 + @side2 > @side1 || @side1 + @side3 > @side2
       raise TriangleError
+    elsif @side1 <= 0 || @side2 <= 0 || @side3 <= 0 
+      raise TriangleError
     elsif  @side1 == @side2 && @side2 == @side3
       :equilateral
     elsif @side1 == @side2 && @side2 != @side3 || @side2 == @side3 && @side2 != @side1 || @side3 == @side1 && @side2 != @side3
      :isosceles
     elsif @side1 != @side2 && @side2 != @side3 || @side1 != @side3
       :scalene
-     
-    elsif @side1 <= 0 || @side2 <= 0 || @side3 <= 0 
-      raise TriangleError
     
     else
       triangle.kind = self
