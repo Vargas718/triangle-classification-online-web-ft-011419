1662093f37324afc16da235fb0d017417fc55240
Rafael Vargas <vargas718@gmail.com>
2019-04-11 16:40:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,13 +10,12 @@ class Triangle
  end 
   
   def kind
-    binding.pry
    if  @side1 == @side2 && @side2 == @side3
       :equilateral
       # elsif @side1 == @side2 && @side2 != @side3 || @side2 == @side2 && @side2 != @side1 || @side3 == @side1 && @side2 != @side3
      
      
-    else if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 
+    elsif @side1 <= 0 || @side2 <= 0 || @side3 <= 0 
       raise Triangleerror
     elsif
       @side1 + @side2 > @side3 || @side3 + @side2 > @side1 || @side1 + @side3 > @side2
